0534c8892ae2506ca99743936793c7d851cde671
andry6197 <andry.nbo@gmail.com>
2026-05-05 17:07:37 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("asl-cache").then(cache => {
-      return cache.addAll(["index.html", "manifest.json"]);
-    })
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
-});
+document.getElementById("detail").innerHTML = `
+  <h2>${d.title}</h2>
+  <div class="price-box">
+    ${d.price}
+    <span class="${changeClass}">${d.change} (${d.changeVal})</span>
+    <span class="currency">${d.currency}</span>
+    <span class="time">${d.time}</span>
+  </div>
+  <iframe src="https://s.tradingview.com/widgetembed/?symbol=${d.symbol}&interval=D&theme=${document.body.classList.contains('dark')?'dark':'light'}"></iframe>
+  ${fundamentalsHTML}
+`;
